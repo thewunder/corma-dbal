6bce060f4386af08c6bfe1b2afcce9c04094ec3a
Michael O'Connell <moconnell@citylifestyle.com>
2024-07-06 20:12:54 -0500

    Replace phpstan and psalm with rector

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/static-analysis',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets()
+    ->withSets([PHPUnitSetList::PHPUNIT_100])
+    ->withTypeCoverageLevel(20)
+    ->withDeadCodeLevel(22);
